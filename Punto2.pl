99af78371f462b53389b1ff3d6c51199cdab1fc7
Ramiro Pinchao <antonio.pinchao@utp.edu.co>
2025-02-20 20:28:31 -0500

    Update Punto2.pl
    Add my version of the complete programme to determine if the Coronel West is a criminal. The program has two rules, and tree facts that help to determine the query.

--- Punto2.pl
+++ Punto2.pl
@@ -1,3 +1,13 @@
 is_from(coronelWest, estadosUnidos).
-enemigo_de(coreDelSur, estadosUnidos).
+enemy_of(estadosUnidos,coreaDelSur).
+sell_weapons(coronelWest, coreaDelSur).
 
+is_hostile_nation(N) :- 
+    enemy_of(estadosUnidos, N).
+
+is_criminal(X) :- 
+    is_from(X, estadosUnidos),
+    sell_weapons(X, coreaDelSur),
+    is_hostile_nation(coreaDelSur).
+    
+    
