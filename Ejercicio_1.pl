04787a3959875e4e3fbd71fb62d46dd6bfcfc04c
kenneth-rr <kenneth.rodriguez@utp.edu.co>
2025-03-25 15:49:21 -0600

    FIX Ejercicio_1.pl
    añado hermana de y hermano de

--- Ejercicio_1.pl
+++ Ejercicio_1.pl
@@ -1,6 +1,12 @@
-progenitor_de( padre(abrahamSimpson), hijo( [ herbertPowell, homeroSimpson ] ) ).
 
-progenitor_de( padre(clancyBouvier), hija( [ margeSimpson, pattyBouvier, selmaBouvier ] ) ).
+
+progenitor_de( padre( abrahamSimpson ), hijo( [ herbertPowel, homeroSimpson ] ) ).
+
+progenitor_de( padre( abrahamSimpson ), hija( [ ] ) ).
+
+progenitor_de( padre( clancyBouvier ), hija( [ margeSimpson, pattyBouvier, selmaBouvier ] ) ).
+
+progenitor_de( padre( clancyBouvier ), hijo( [ ] ) ).
 
 progenitor_de( padre( homeroSimpson ), hijo( [ bartSimpson ] ) ).
 
@@ -8,13 +14,20 @@ progenitor_de( padre( homeroSimpson ), hija( [ lisaSimpson, maggieSimpson ] ) ).
 
 progenitor_de( madre( monaSimpson ), hijo( [ herbertPowel, homeroSimpson ] ) ).
 
+progenitor_de( madre( monaSimpson ), hija( [ ] ) ).
+
 progenitor_de( madre( jacquelineBouvier ), hija( [ margeSimpson, pattyBouvier, selmaBouvier ] ) ).
+
+progenitor_de( madre( jacquelineBouvier ), hijo( [ ] ) ).
 
 progenitor_de( madre( margeSimpson ), hijo( [ bartSimpson ] ) ).
 
 progenitor_de( madre( margeSimpson ), hija( [ lisaSimpson, maggieSimpson ] ) ).
 
 progenitor_de( madre( selmaBouvier ), hija( [ lingBouvier ] ) ).
+
+progenitor_de( madre( selmaBouvier ), hijo( [ ] ) ).
+
 
 abuelo_de(X, Y):-
     progenitor_de( padre(X), hijo(Hijos) ), member( Z, Hijos ),
@@ -41,17 +54,18 @@ abuela_de(X, Y):-
     ).
 
 hermano_de(X, Y):-
-    progenitor_de( P, hijo( Hijos ) ), member( X, Hijos ), progenitor_de( P, hija( Hijas ) ),
+    progenitor_de( P, hijo(Hijos) ), progenitor_de( P, hija( Hijas ) ), member( X, Hijos ), 
     ( member( Y, Hijos ) ; member( Y, Hijas ) ), X \= Y.
-    
-    %padre_de( _, Hijos1), member( Y, Hijos1 ), member( X, Hijos1 ), X \= Y, sexo(Hombres, masculino), member( X, Hombres ) .
 
 hermana_de(X, Y):-
-    padre_de( _, Hijos1), member( Y, Hijos1 ), member( X, Hijos1 ), X \= Y, sexo(Mujeres, femenino), member( X, Mujeres ) .
+    progenitor_de( P, hijo(Hijos) ), progenitor_de( P, hija( Hijas ) ), member( X, Hijas ), 
+    ( member( Y, Hijos ) ; member( Y, Hijas ) ), X \= Y.
 
 tio_de(X, Y):-
-    ( madre_de( Madre, Hijos1 ), member( Y, Hijos1 ), ( hermano_de(X, Madre) ; hermana_de(X, Madre) ) ) ;
-    ( padre_de( Padre, Hijos2 ), member( Y, Hijos2 ), ( hermano_de(X, Padre) ; hermana_de(X, Padre) ) ).
+    
+    
+    %( madre_de( Madre, Hijos1 ), member( Y, Hijos1 ), ( hermano_de(X, Madre) ; hermana_de(X, Madre) ) ) ;
+    %( padre_de( Padre, Hijos2 ), member( Y, Hijos2 ), ( hermano_de(X, Padre) ; hermana_de(X, Padre) ) ).
 
 primo_de(X, Y):-
     tio_de(Progenitor, Y),( padre_de( Progenitor, Hijos2 ), member( X, Hijos2 ) ;  madre_de( Progenitor, Hijos1 ), member( X, Hijos1 ) ), X \= Y.
